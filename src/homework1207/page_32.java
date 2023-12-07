be9c3851efa769b6bfb1d3e6d316dac6f37b8397
Yeeming0622 <asdasd121232145@gmail.com>
2023-12-07 14:04:34 +0800

    2023_12_07

---
+++ src/homework1207/page_32.java
@@ -0,0 +1,11 @@
+package homework1207;
+
+public class page_32 {
+	public static void main(String[] args)throws CarException 
+	{
+		Car30 car1 = new Car30();
+		
+		car1.setCar30(1234, -10.0);
+		car1.show();
+	}
+}
---
+++ src/pb/page_14.java
@@ -0,0 +1,11 @@
+package pb;
+
+import pc.Car;
+
+public class page_14 {
+	public static void main(String[] args)
+	{
+		Car car1 = new Car();
+		car1.show();
+	}
+}
